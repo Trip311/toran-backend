4c93022b1da20be27bd3fcad68d2e861e1910924
Trip311 <elad.alayoff1@gmail.com>
2025-05-26 13:50:44 +0300

    removed the api

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,4 +1,4 @@
-import { MoreThan } from "typeorm";
+
 import { IEvent } from "../interfaces/event.interface";
 import { EventRepo } from "../repository/event.repository";
 
@@ -20,15 +20,3 @@ export const deleteEvent = async (id:number) => {
     return await EventRepo.delete(id);
 }
 
-export const getEventsByFilters = async (username: string, type: string) => {
-    const now = new Date();
-    const where: any = {
-        username,
-        date: MoreThan(now)
-    };
-    if (type) {
-        where.type = type;
-    }
-
-    return await EventRepo.find({ where });
-};
