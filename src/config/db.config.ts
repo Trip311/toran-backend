d61cf4c164f5824213ee15b1cf7f15d9531928f6
elad1122 <elad.alayoff1@gmail.com>
2025-05-31 12:26:35 +0300

    specify to ts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,30 +1,3 @@
-// import { DataSource } from "typeorm";
-// import "reflect-metadata";
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
-
-
-// export const dbConnection = new DataSource({
-//     type: "postgres",
-//     host: DB_HOST,
-//     port: Number(DB_PORT),
-//     username: DB_USERNAME,
-//     password: DB_PASSWORD,
-//     database: DB_NAME,
-//     synchronize: true,
-//     entities: ['src/entity/*.ts']
-// });
-
-//  export const initialize = async () => {
-//     await dbConnection.initialize().catch((err: any) => {
-//         console.error("Error with db connection", err);
-//     })
-//         console.info("Connection with DB established");
-//  }
-
 
 import { DataSource } from "typeorm";
 import "reflect-metadata";
